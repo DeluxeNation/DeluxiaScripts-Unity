cedba45112737b804b2246f15508fd96b7a761b9
DeluxeNation <Dragonman0901@gmail.com>
2024-05-18 22:55:28 -0500

    New SetPivot method

--- DeluxiaScripts-Unity/DeluxiaUnityMethods.cs
+++ DeluxiaScripts-Unity/DeluxiaUnityMethods.cs
@@ -798,5 +798,16 @@ namespace Deluxia.Unity{
 				yield return current;
 			}
 		}
+		//https://discussions.unity.com/t/set-a-recttranforms-pivot-without-changing-its-position/139741/2
+		public static void SetPivot(this RectTransform rectTransform,Vector2 pivot) {
+			if(rectTransform == null)
+				return;
+
+			Vector2 size = rectTransform.rect.size;
+			Vector2 deltaPivot = rectTransform.pivot - pivot;
+			Vector3 deltaPosition = new Vector3(deltaPivot.x * size.x,deltaPivot.y * size.y);
+			rectTransform.pivot = pivot;
+			rectTransform.localPosition -= deltaPosition;
+		}
 	}
 }
