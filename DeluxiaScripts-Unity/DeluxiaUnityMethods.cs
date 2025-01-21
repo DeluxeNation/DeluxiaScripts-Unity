b9d5bbeac339d3e9b50f2edbd52ae80bd1f462e0
DeluxeNation <91623968+DeluxeNation@users.noreply.github.com>
2025-01-20 21:45:28 -0600

    Added GetPath to get path of Transform

--- DeluxiaScripts-Unity/DeluxiaUnityMethods.cs
+++ DeluxiaScripts-Unity/DeluxiaUnityMethods.cs
@@ -743,5 +743,10 @@ namespace Deluxia.Unity{
 			rectTransform.pivot = pivot;
 			rectTransform.localPosition -= deltaPosition;
 		}
+        public static string GetPath(this Transform current) {
+            if (current.parent == null)
+                return "/" + current.name;
+            return current.parent.GetPath() + "/" + current.name;
+        }
 	}
 }
